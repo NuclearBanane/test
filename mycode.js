59a00bac2afa92cfb3ea09691dcc209da7eee3f3
t0xicCode <xavier.l@t0xic.co.de>
2016-03-03 20:30:33 -0500

    Subbing my world!

--- mycode.js
+++ mycode.js
@@ -1,6 +1,5 @@
 // Various functions to demonstrate merge conflicts, and resolutions
-
-/* Version: v0.2
+/* Version: v0.3
  * Author : NuclearBanane
  * Email  : nuclearbanane@gmail.com
  * Contributors : Stickler (Stickler@gmail.com) 
@@ -10,8 +9,21 @@
  * 	Returns n-th number in the Fibonacci series
  *
  * Author : NuclearBanane
- * Contributers : Stickler
+ * Contributors : Stickler
+ * Version : v0.1.2
  */
 function fib(n){
-    return (n==0 || n==1) ? 1 : fib(n-1) + fib(n-2);
+	return (n==0 || n==1) ? 1 : fib(n-1) + fib(n-2);
+}
+
+/* Subber function
+ * 	returns the result of the substraction a - b
+ *
+ * 
+ * Author : Stickler
+ * Contributors : 
+ * Version : v0.1
+ */
+function sub(a,b) {
+	return a-b;
 }
