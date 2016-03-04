dd5289a31d09115166b6d85ee7f89ab7725635d5
Alex Dale <adale031@uottawa.ca>
2016-03-03 20:30:30 -0500

    Adding a adder function

--- mycode.js
+++ mycode.js
@@ -1,17 +1,29 @@
 // Various functions to demonstrate merge conflicts, and resolutions
-
-/* Version: v0.2
+/* Version: v0.3
  * Author : NuclearBanane
  * Email  : nuclearbanane@gmail.com
  * Contributors : Stickler (Stickler@gmail.com) 
  */ 
 
 /* Fibonacci Series
- * 	Returns n-th number in the Fibonacci series
+ *  Returns n-th number in the Fibonacci series
  *
  * Author : NuclearBanane
- * Contributers : Stickler
+ * Contributors : Stickler
+ * Version : v0.1.2
  */
 function fib(n){
     return (n==0 || n==1) ? 1 : fib(n-1) + fib(n-2);
 }
+
+/* Adder function
+ *  returns the result of the addition a + b
+ * 
+ * Author : NuclearBanane
+ * Contributors : 
+ * Version : v0.1
+ */ 
+function add(a,b) {
+    return a+b;
+}
+
